8d0204c6b195f2bd42c413a8fbed449a618b527b
jhonolvera <71787917+jhonolvera@users.noreply.github.com>
2020-12-05 16:42:39 -0600

    Add files via upload

---
+++ lista_burbu.cpp
@@ -0,0 +1,51 @@
+#include <iostream> 
+using namespace std;
+#include <cstdlib>
+
+int const DIM = 10;
+typedef float tArray[DIM];
+
+void rellenarArray(tArray);
+void ordenarArray(tArray);
+void mostrarArray(const tArray);
+
+int main(){
+	tArray miArray;
+	
+	rellenarArray(miArray);
+	ordenarArray(miArray);
+	mostrarArray(miArray);
+	
+	system("PAUSE");
+	return 0;
+}
+
+void rellenarArray(tArray miArray){
+	cout << "Introduzca los 10 elementos del arreglo" << endl;
+	
+	for (int i = 0; i < DIM; i++){ 
+		cin >> miArray[i];
+	}
+}
+
+void ordenarArray(tArray miArray){
+	float temporal;
+	
+	for (int i = 0;i < DIM; i++){
+		for (int j = 0; j< DIM-1; j++){
+			if (miArray[j] < miArray[j+1]){ 
+			temporal = miArray[j]; 
+			miArray[j] = miArray[j+1]; 
+			miArray[j+1] = temporal;
+			}
+		}
+	}
+}
+
+void mostrarArray(const tArray miArray){
+	cout << "Mostrando array ordenado..." << endl;
+	
+	for (int i = 0; i < DIM; i++) 
+		cout << miArray[i] << endl;
+		
+}
